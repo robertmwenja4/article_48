16b8cfe542a08c27306d99201ce48fbafa08da57
robertmwenja4 <robertmwenja4@gmail.com>
2025-08-11 13:42:21 +0300

    Modification for access on json to js files

--- js/latest_news.js
+++ js/latest_news.js
@@ -1,29 +1,21 @@
-fetch('../news_items.json')
-.then(response => response.json())
-.then(newsItems => {
-    // Example: Only show first 2 items
-    const limitedItems = newsItems.slice(0, 3); 
 
-    // OR: Filter by ID
-    // const filteredItem = newsItems.find(item => item.id === 1);
+const container = document.getElementById('news-cards');
 
-    // Then display your content
-    const container = document.getElementById('news-cards');
-    limitedItems.forEach(item => {
+newsData.forEach(item => {
     const col = document.createElement('div');
     col.className = "col-lg-4 col-md-6 col-12 mb-4";
     col.innerHTML = `
         <div class="custom-block-wrap">
-        <img src="${item.image}" class="custom-block-image img-fluid" alt="">
-        <div class="custom-block">
-            <div class="custom-block-body">
-            <h5 class="mb-3">${item.title}</h5>
-            <p>${item.summary}</p>
+            <img src="${item.image}" class="custom-block-image img-fluid" alt="">
+            <div class="custom-block">
+                <div class="custom-block-body">
+                    <h5 class="mb-3">${item.title}</h5>
+                    <p>${item.summary}</p>
+                </div>
+                <a href="gender_disability.html?id=${item.id}" class="custom-btn btn">Read More</a>
             </div>
-            <a href="gender_disability.html?id=${item.id}" class="custom-btn btn">Read More</a>
-        </div>
         </div>
     `;
     container.appendChild(col);
-    });
 });
+
